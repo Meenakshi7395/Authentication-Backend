5e558b9dfeda5913bc3fa9ea62f8b018b1e6d5c2
Meenakshi <meenakshisharma7395@gmail.com>
2024-12-04 00:57:46 +0530

    updated

--- index.js
+++ index.js
@@ -19,12 +19,26 @@ mongoose.connect('mongodb://localhost:27017/Authentication_form', {
 
 import users from "./routes/users.js"
 app.use(bodyParser.json());
+var homePage = `
+<h1>Welcome to Authentication Form Backend</h1>
+<h2>Main Features of Authentication Form</h2>
+<ul>
+<li>Login controllers are defined.</li>
+<li>User Schema is defined.</li>
+<li>MongoDB data base is used.</li>
+<li>For User details , createUser, ViewUser by Id , APIs are created. </li>
+</ul>
+<p>Please click <a href="https://authentication-frontend-d0wg.onrender.com/">here</a> For Better User Experience</p>
+<p>Thanks for checking it out</p>
+`
 
 
-// app.get('/',(req,res) => {
-//     res.send({name:"Hello world"})
-// });
-  
+app.get('/',(req,res) => {
+    // res.send({name:"Hello world"})
+    res.send(homePage);
+
+});
+
 // Routes
 app.use('/users', users);
 
